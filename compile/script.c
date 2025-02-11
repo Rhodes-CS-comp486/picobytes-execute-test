09bfc27d00186ae9a0ad54c66fb814a159e0b325
Shishir <theshishir01@gmail.com>
2025-02-11 16:46:48 -0600

    basic testing

---
+++ compile/script.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "script.h"
+
+
+int run(void) {
+  printf("Hello, world! I'm running!\n");
+  return 0;
+}
---
+++ compile/script.h
@@ -0,0 +1,6 @@
+#ifndef SCRIPT_H
+#define SCRIPT_H
+
+int run(void);
+
+#endif
