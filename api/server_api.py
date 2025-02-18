2063cb946160d4f2efa4da1b3075d5e054ff3bd4
Liam Kressin <liamkressin@gmail.com>
2025-02-18 09:58:12 -0600

    add another endpoint to handle base64 encoded json and write it to a file

--- api/server_api.py
+++ api/server_api.py
@@ -1,3 +1,5 @@
+from base64 import b64decode
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 import subprocess
@@ -5,11 +7,14 @@ import tempfile
 import json
 from pathlib import Path
 import uvicorn
+import base64
 
 directory = Path("to_execute")
 directory.mkdir(parents=True, exist_ok=True)
 filepath1 = directory / "code.c"
 filepath2 = directory / "tests.c"
+filepath3 = directory / "ecode.c"
+filepath4 = directory / "etests.c"
 
 class Item(BaseModel):
     code : str
@@ -34,8 +39,18 @@ def better_submit(item : Item):
     else:
         with filepath2.open( "w", encoding="utf-8") as f:
             f.write("")
-
-
+@app.post("/encoded")
+def decode_and_write(item: Item):
+    dcode = b64decode(item.code).decode("utf-8")
+    with filepath3.open( "w", encoding="utf-8") as f:
+        f.write(dcode)
+    if item.tests is not None:
+        dtests = b64decode(item.tests).decode("utf-8")
+        with filepath4.open( "w", encoding="utf-8") as f:
+            f.write(dtests)
+    else:
+        with filepath4.open( "w", encoding="utf-8") as f:
+            f.write("")
 
 if __name__ == "__main__":
     uvicorn.run("server_api:app", host="127.0.0.1", port=5000, reload=True)
--- api/test_api.py
+++ api/test_api.py
@@ -5,16 +5,26 @@ from fastapi.testclient import TestClient
 from server_api import app
 
 client = TestClient(app)
-filepath = Path(__file__).parent / "testcode.json"
-""
+filepath1 = Path(__file__).parent / "testcode.json"
+filepath2 = Path(__file__).parent / "encodedtest.json"
+
 try:
-    with open(filepath, "r", encoding="utf-8") as f:
+    with open(filepath1, "r", encoding="utf-8") as f:
         data = json.load(f)
 
 except (FileNotFoundError, json.JSONDecodeError) as e:
     raise RuntimeError(f"Error reading test file: {e}")
 ccode = data.get("code", "")
 tests = data.get("tests")
+
+try:
+    with open(filepath2, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+except (FileNotFoundError, json.JSONDecodeError) as e:
+    raise RuntimeError(f"Error reading test file: {e}")
+ecode = data.get("code", "")
+etests = data.get("tests")
 
 def test_api_submit_with_test():
     json_payload = {"code": ccode, "tests": tests}
@@ -27,3 +37,12 @@ def test_api_submit_no_test():
     response = client.post("/submit", json=json_payload)
     assert response.status_code == 200
 
+def test_encoded_json_with_test():
+    json_payload = {"code": ecode, "tests": etests}
+    response = client.post("/encoded", json=json_payload)
+    assert response.status_code == 200
+
+def test_encoded_json_no_test():
+    json_payload = {"code": ecode}
+    response = client.post("/encoded", json=json_payload)
+    assert response.status_code == 200
