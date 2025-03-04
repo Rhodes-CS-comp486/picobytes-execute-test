212ad7047ba56f8d7cf9a5ba886f51ae644d72dd
Shishir <theshishir01@gmail.com>
2025-03-04 01:23:11 -0600

    docker running version

---
+++ api/external_test.py
@@ -0,0 +1,24 @@
+import json
+
+filepath1 = "./testcode.json"
+filepath2 = "./encodedtest.json"
+
+try:
+    with open(filepath1, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+except (FileNotFoundError, json.JSONDecodeError) as e:
+    raise RuntimeError(f"Error reading test file: {e}")
+ccode = data.get("code", "")
+tests = data.get("tests")
+
+json_payload = {"code": ccode, "tests": tests}
+
+import requests
+response = requests.post("http://127.0.0.1:5000/submit", json=json_payload)
+
+
+print(response.json())
+
+
+
--- api/server_api.py
+++ api/server_api.py
@@ -67,4 +67,4 @@ def decode_and_write(item: Item):
             f.write("")
 
 if __name__ == "__main__":
-    uvicorn.run("server_api:app", host="127.0.0.1", port=5000, reload=True)
+    uvicorn.run("server_api:app", host="0.0.0.0", port=5000, reload=True)
